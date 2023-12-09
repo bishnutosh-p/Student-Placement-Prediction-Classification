d8064d4f5b3962a3d45568822de5f677f191fe60
Bishnutosh <70653087+bishnu9009@users.noreply.github.com>
2023-12-09 19:27:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("Hello world!")
+def main():
+    st.title("Streamlit App with 10 Inputs and 1 Output")
+
+    # Create 10 input fields
+    input_values = [st.number_input(f"Input {i+1}", value=0.0) for i in range(10)]
+
+    # Calculate the sum of the input values
+    output_value = sum(input_values)
+
+    # Display the output
+    st.write(f"Output: {output_value}")
+
+if __name__ == "__main__":
+    main()
